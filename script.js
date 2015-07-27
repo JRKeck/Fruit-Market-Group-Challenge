a3e5173b57433aab1683a8bb674fbd996d7888c0
JRKeck <joshrkeck@gmail.com>
2015-07-26 23:13:17 -0500

    Objectify

--- script.js
+++ script.js
@@ -1,147 +1,111 @@
+$(document).ready(function(){
+//Array containing the store Objects
+var storeInventory = [];
+
+//Create Objects for the store items and push them to the storeInventory array
+storeInventory.push(new StoreItem ("Apple", "apple.png"));
+storeInventory.push(new StoreItem ("Bananas", "bananas.png"));
+storeInventory.push(new StoreItem ("Orange", "orange.png"));
+storeInventory.push(new StoreItem ("Pear", "pear.png"));
+
+//Create an Object for the user
+var User = {
+	cash : 10000
+}
+
+//populate screen with the storeInventory
+for (var i = 0; i < storeInventory.length; i++) {
+	var price = formatNum(storeInventory[i].currentPrice);
+	$('.store').append('<div class="col-md-3 col-xs-6 store-item '+storeInventory[i].name+'"></div>');//add store-item container
+	$('.store > .'+storeInventory[i].name).append('<img src="images/'+storeInventory[i].img+'">');//add item image
+	$('.store > .'+storeInventory[i].name).append('<h3 class="name">'+storeInventory[i].name+'</h3>');
+	$('.store > .'+storeInventory[i].name).append('<h3 class="price">'+price+'</h3>');
+	$('.store > .'+storeInventory[i].name).append('<button class="btn buy" id="'+storeInventory[i].name+'">Buy</button>');
+	$('.store > .'+storeInventory[i].name).append('<button class="btn sell" id="'+storeInventory[i].name+'">Sell</button>');
+};
+//populate screen with user basket
+for (var i = 0; i < storeInventory.length; i++) {
+	$('.cart').append('<div class="col-sm-12 col-xs-12 cart-item '+storeInventory[i].name+'"></div>');//add cart-item container
+	$('.cart > .'+storeInventory[i].name).append('<div class="num-in-cart">'+storeInventory[i].numInCart+'</div>');//add item image
+	$('.cart > .'+storeInventory[i].name).append('<div class=""><img src="images/'+storeInventory[i].img+'"></div>');//add item image
+	$('.cart > .'+storeInventory[i].name).append('<div class="avg-price"></div>');//add average price text
+
+
+};
+$('.cart').append('<div class="col-md-12 col-sm-6 col-xs-12 user-cash">Total Cash: '+formatNum(User.cash)+'</div>');//add cart-item container	
+
+
+//Store Item consructor
+function StoreItem(name, img) {
+	this.name = name;
+	this.img = img
+	this.numPurchased = 0; //total number purchased during game
+	this.numInCart = 0; //current number in users cart
+	this.totalAmtSpent = 0; //total amount spent on this fruit
+	this.currentPrice = randomNumber(50, 999); //set current price of fruit $.50 - $9.99
+	this.priceTrend = ""; //track the price trend	
+}
+
+//update the price of the fruit
+function updateFruitPrice(price, findStoreItem) {
+	var randomChange = randomNumber(1, 2);
+	if (price > 949) {price -= 50; } //avoid price over $9.99
+	else if (price < 100) {price += 50} //avoid price under $.50
+	else if (randomChange === 1) {price += 50}// add $.50
+	else {price -= 50} //subtract $.50
+		return price;	
+}
+//buy a storeItem
+$('body').on('click', '.buy', function() {
+	var getBtnId = $(this).attr('id');
+	var findStoreItem = $.grep(storeInventory, function(e){ return e.name == getBtnId});
+	if (User.cash >= findStoreItem[0].currentPrice){
+		findStoreItem[0].numInCart++;
+		$('.'+getBtnId+' .num-in-cart').text(findStoreItem[0].numInCart);//update cart inventory on DOM	
+		findStoreItem[0].numPurchased++;
+		findStoreItem[0].totalAmtSpent += findStoreItem[0].currentPrice;
+		User.cash -= findStoreItem[0].currentPrice; //addjust user cash
+		$('.cart .user-cash').text('Total Cash: '+formatNum(User.cash));//update user cash on DOM
+		var avgPrice = (findStoreItem[0].totalAmtSpent/findStoreItem[0].numPurchased);
+		$('.'+getBtnId+' .avg-price').text("Avg. purchase price: "+formatNum(avgPrice));//update cart inventory on DOM	
+		console.log(avgPrice);	
+	}
+});
+//sell a storeItem
+$('body').on('click', '.sell', function() {
+	var getBtnId = $(this).attr('id');
+	var findStoreItem = $.grep(storeInventory, function(e){ return e.name == getBtnId});
+	if (findStoreItem[0].numInCart > 0){//make sure there are storeItems to sell
+		findStoreItem[0].numInCart--;
+		$('.'+getBtnId+' .num-in-cart').text(findStoreItem[0].numInCart);//update cart inventory on DOM	
+		User.cash += findStoreItem[0].currentPrice;
+		$('.cart .user-cash').text('Total Cash: '+formatNum(User.cash));//update user cash on DOM	
+	}
+	
+});
+
+//this will call updateFruitPrice every 1 seconds
+setInterval(function(){
+	for (var i = 0; i < storeInventory.length; i++) {
+		storeInventory[i].currentPrice = updateFruitPrice(storeInventory[i].currentPrice, storeInventory[i].name)//update fruit price
+		$('.'+storeInventory[i].name+' > .price').text(formatNum(storeInventory[i].currentPrice));//send new price to the dom 
+	};
+}, 1000);
+
+//format to Dollar values
+function formatNum(num) {
+	num /= 100;
+	num = parseFloat(Math.round(num * 100) / 100).toFixed(2);
+	num = num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	num = "$" + num;
+	return num;
+}
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 
 }
+}); //End Document Ready
 
-var User = {
-	numCurrentApples : 0,
-	numCurrentOranges : 0,
-	numCurrentBananas : 0,
-	//numCurrentGrapes : 0,
-	numCurrentPears : 0,
-	totalNumApples : 0,
-	totalNumOranges : 0,
-	totalNumBananas : 0,
-	//totalNumGrapes : 0,
-	totalNumPears : 0,
-	spentApples : 0,
-	spentOranges : 0,
-	spentBananas : 0,
-	//spentGrapes : 0,
-	spentPears : 0,
-	cash : 10000
-}
-
-//this is user inventory
-var userBasket = {
-	apples: 0,
-	oranges: 0,
-	bananas: 0,
-	grapes: 0,
-	pears: 0
-}
-
-//something that defines fruit prices' starting point
-	//[2] will indicate if price has gone up or down in last fiftyCents function call
-var fruitPrices = [
-	["apples", 50, ""], 
-	["oranges", 50, ""],
-	["bananas", 50, ""],
-	//["grapes", 950, ""],
-	["pears", 950, ""]
-]
-
-var change = "";
-
-//function that changes price for each fruit
-function fruitPriceUpdates() {
-	for(var i = 0; i < fruitPrices.length; i++) {
-		if(randomNumber(1, 2) == 1 && (fruitPrices[i][1] <= 900)){
-			fruitPrices[i][1] += 50;
-			fruitPrices[i][2] = "increase";
-			// console.log("increase");
-		} else if (fruitPrices[i][1] > 50) {
-			fruitPrices[i][1] -= 50;
-			fruitPrices[i][2] = "decrease";
-			// console.log("decrease");
-		};
-	}
-	$('.apple > .price').text("$" + fruitPrices[0][1]);
-	$('.orange > .price').text("$" + fruitPrices[1][1]);
-	$('.banana > .price').text("$" + fruitPrices[2][1]);
-	$('.pear > .price').text("$" + fruitPrices[3][1]);
-// console.log("pears price: " + fruitPrices[1][1] + "pears indicator: " + fruitPrices[1][2]);
-}
-fruitPriceUpdates(fruitPrices);
-//this will call the fruitPriceUpdates function each 15 seconds
-setInterval('fruitPriceUpdates(fruitPrices);', 1000);
-
-$(document).ready(function(){
-
-	$('.cash').text("$" + User.cash);
-	$(".buy-apple").on('click', function(){
-		User.numCurrentApples++;
-		User.totalNumApples++;
-		User.spentApples += fruitPrices[0][1];
-		User.cash -= fruitPrices[0][1];
-	var avgApple = User.spentApples / User.totalNumApples;
-	$('.userapple > .inventory').text(User.numCurrentApples);
-	$('.userapple > .avgprice').text(avgApple);
-	$('.cash').text("$" + User.cash);
-	});
-	
-	$(".buy-orange").on('click', function(){
-		User.numCurrentOranges++;
-		User.totalNumOranges++;
-		User.spentOranges += fruitPrices[1][1];
-		User.cash -= fruitPrices[1][1];
-	var avgOrange = User.spentOrange / User.totalNumOranges;
-	$('.userorange > .inventory').text(User.numCurrentOranges);
-	$('.userorange > .avgprice').text(avgOrange);
-	$('.cash').text("$" + User.cash);
-	});
-	
-	$(".buy-banana").on('click', function(){
-		User.numCurrentBananas++;
-		User.totalNumBananas++;
-		User.spentBananas += fruitPrices[2][1];
-		User.cash -= fruitPrices[2][1];
-	var avgBanana = User.spentBananas / User.totalNumBananas;
-	$('.userbanana > .inventory').text(User.numCurrentBananas);
-	$('.userbanana > .avgprice').text(avgBanana);
-	$('.cash').text("$" + User.cash);
-	});
-	
-	$(".buy-pear").on('click', function(){
-		User.numCurrentPears++;
-		User.totalNumPears++;
-		User.spentPears += fruitPrices[3][1];
-		User.cash -= fruitPrices[3][1];
-	var avgPear = User.spentPears / User.totalNumPears;
-	$('.userpear > .inventory').text(User.numCurrentPears);
-	$('.userpear > .avgprice').text(avgPear);
-	$('.cash').text("$" + User.cash);
-	});
-	
-	$(".sell-apple").on('click', function(){
-		User.numCurrentApples--;
-		User.cash += fruitPrices[0][1];
-	$('.userapple > .inventory').text(User.numCurrentApples);
-	$('.cash').text("$" + User.cash);
-	});
-	
-	$(".sell-orange").on('click', function(){
-		User.numCurrentOranges--;
-		User.cash += fruitPrices[1][1];
-	$('.userorange > .inventory').text(User.numCurrentOranges);
-	$('.cash').text("$" + User.cash);
-	});
-	
-	$(".sell-banana").on('click', function(){
-		User.numCurrentBananas--;
-		User.cash += fruitPrices[2][1];
-	$('.userbanana > .inventory').text(User.numCurrentBananas);
-	$('.cash').text("$" + User.cash);
-	});
-	
-	$(".sell-pear").on('click', function(){
-		User.numCurrentPears--;
-		User.cash += fruitPrices[3][1];
-	$('.userpear > .inventory').text(User.numCurrentPears);
-	$('.cash').text("$" + User.cash);
-	});
-
-}); 
 
 
 
