442e4f00537841f2cd24bad6696835a52fa85a18
JRKeck <joshrkeck@gmail.com>
2015-07-30 20:56:41 -0500

    Add game start and end

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 //Array containing the store Objects
+var priceChangeInterval = 5000; // In milleseconds 5000 = 5 seconds
+var gameLength = 20000;// In milleseconds, 120000 = 2 minutes
+var gameTime = 0; // Keep track of how long the game has been running
+var gameValid = false;//Set false when the game is over
 var storeInventory = [];
 
 //Create Objects for the store items and push them to the storeInventory array
@@ -12,7 +16,11 @@ storeInventory.push(new StoreItem ("Pear", "pear.png"));
 var User = {
 	cash : 10000
 }
-
+$('#startModal').modal({
+	backdrop: 'static',
+	keyboard: false
+})
+$('#startModal').modal('show');
 //populate screen with the storeInventory
 for (var i = 0; i < storeInventory.length; i++) {
 	var price = formatNum(storeInventory[i].currentPrice);
@@ -32,7 +40,7 @@ for (var i = 0; i < storeInventory.length; i++) {
 
 
 };
-$('.cart').append('<div class="col-md-12 col-sm-6 col-xs-12 user-cash">Total Cash: '+formatNum(User.cash)+'</div>');//add cart-item container	
+$('.cart').append('<div class="col-md-12 col-sm-6 col-xs-12 user-cash">Total Cash: '+formatNum(User.cash)+'</div>');//add cart-item container
 
 
 //Store Item consructor
@@ -43,7 +51,7 @@ function StoreItem(name, img) {
 	this.numInCart = 0; //current number in users cart
 	this.totalAmtSpent = 0; //total amount spent on this fruit
 	this.currentPrice = randomNumber(50, 999); //set current price of fruit $.50 - $9.99
-	this.priceTrend = ""; //track the price trend	
+	this.priceTrend = true; //track the price trend
 }
 
 //update the price of the fruit
@@ -53,44 +61,81 @@ function updateFruitPrice(price, findStoreItem) {
 	else if (price < 100) {price += 50} //avoid price under $.50
 	else if (randomChange === 1) {price += 50}// add $.50
 	else {price -= 50} //subtract $.50
-		return price;	
+		return price;
 }
 //buy a storeItem
 $('body').on('click', '.buy', function() {
-	var getBtnId = $(this).attr('id');
-	var findStoreItem = $.grep(storeInventory, function(e){ return e.name == getBtnId});
-	if (User.cash >= findStoreItem[0].currentPrice){
-		findStoreItem[0].numInCart++;
-		$('.'+getBtnId+' .num-in-cart').text(findStoreItem[0].numInCart);//update cart inventory on DOM	
+	if (gameValid == true){
+		var getBtnId = $(this).attr('id');
+		var findStoreItem = $.grep(storeInventory, function(e){ return e.name == getBtnId});
+		if (User.cash >= findStoreItem[0].currentPrice){
+			findStoreItem[0].numInCart++;
+		$('.'+getBtnId+' .num-in-cart').text(findStoreItem[0].numInCart);//update cart inventory on DOM
 		findStoreItem[0].numPurchased++;
 		findStoreItem[0].totalAmtSpent += findStoreItem[0].currentPrice;
 		User.cash -= findStoreItem[0].currentPrice; //addjust user cash
 		$('.cart .user-cash').text('Total Cash: '+formatNum(User.cash));//update user cash on DOM
 		var avgPrice = (findStoreItem[0].totalAmtSpent/findStoreItem[0].numPurchased);
-		$('.'+getBtnId+' .avg-price').text("Avg. purchase price: "+formatNum(avgPrice));//update cart inventory on DOM	
-		console.log(avgPrice);	
+		$('.'+getBtnId+' .avg-price').text("Avg. purchase price: "+formatNum(avgPrice));//update cart inventory on DOM
+		console.log(avgPrice);
 	}
+}
 });
 //sell a storeItem
 $('body').on('click', '.sell', function() {
-	var getBtnId = $(this).attr('id');
-	var findStoreItem = $.grep(storeInventory, function(e){ return e.name == getBtnId});
-	if (findStoreItem[0].numInCart > 0){//make sure there are storeItems to sell
-		findStoreItem[0].numInCart--;
-		$('.'+getBtnId+' .num-in-cart').text(findStoreItem[0].numInCart);//update cart inventory on DOM	
-		User.cash += findStoreItem[0].currentPrice;
-		$('.cart .user-cash').text('Total Cash: '+formatNum(User.cash));//update user cash on DOM	
+	if (gameValid == true){
+		var getBtnId = $(this).attr('id');
+		var findStoreItem = $.grep(storeInventory, function(e){ return e.name == getBtnId});
+		if (findStoreItem[0].numInCart > 0){//make sure there are storeItems to sell
+			findStoreItem[0].numInCart--;
+			$('.'+getBtnId+' .num-in-cart').text(findStoreItem[0].numInCart);//update cart inventory on DOM
+			User.cash += findStoreItem[0].currentPrice;
+			$('.cart .user-cash').text('Total Cash: '+formatNum(User.cash));//update user cash on DOM
+		}
 	}
-	
+});
+$('body').on('click', '.btn-start-game', function() {
+	gameValid = true;
+	startGame();
+	console.log("Close Modal");
 });
 
-//this will call updateFruitPrice every 1 seconds
-setInterval(function(){
-	for (var i = 0; i < storeInventory.length; i++) {
+//this will call updateFruitPrice at a set interval determined by priceChangeInterval
+function startGame() {
+	var runGame = setInterval(function(){
+		for (var i = 0; i < storeInventory.length; i++) {
 		storeInventory[i].currentPrice = updateFruitPrice(storeInventory[i].currentPrice, storeInventory[i].name)//update fruit price
-		$('.'+storeInventory[i].name+' > .price').text(formatNum(storeInventory[i].currentPrice));//send new price to the dom 
+		$('.'+storeInventory[i].name+' > .price').text(formatNum(storeInventory[i].currentPrice));//send new price to the dom
 	};
-}, 1000);
+	gameTime += priceChangeInterval;
+	if (gameTime >= gameLength) {
+		clearInterval(runGame);
+		gameValid = false;
+		endGameTally();
+		console.log("The Game is over");
+		}
+
+	}, priceChangeInterval);
+}
+//Sell the users fruit at the end of the game
+function endGameTally() {
+	console.log(storeInventory);
+	for (var i = 0; i < storeInventory.length; i++) {
+		User.cash += (storeInventory[i].numInCart * storeInventory[i].currentPrice)
+	}
+	$('.cart .user-cash').text('Total Cash: '+formatNum(User.cash));//update user cash on DOM
+	if (User.cash >= 10000){
+	$('#endModal .modal-body').append("<p>Congrats you didn't lose any money! You finished the game with "+formatNum(User.cash)+"</p>");
+	}
+	else {
+	$('#endModal .modal-body').append("<p>Oh no, you lost money! You finished the game with "+formatNum(User.cash)+"</p>");
+	}
+	$('#endModal').modal({
+		backdrop: 'static',
+		keyboard: false
+	})
+	$('#endModal').modal('show');
+}
 
 //format to Dollar values
 function formatNum(num) {
